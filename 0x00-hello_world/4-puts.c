27ad5f2e8d1e42d1eff41c5ce1bdc99f60b31a00
HGTpc <kamsidavid2@gmail.com>
2023-03-16 23:56:11 +0000

    done

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
-
+/**
+ * main - prints a string
+ */
 int main(void)
 {
-	char word[] = "Programming is like building a multilingual puzzle\n";
+	char word[] = "\"Programming is like building a multilingual puzzle\n";
+
 	puts(word);
 	return (0);
 
