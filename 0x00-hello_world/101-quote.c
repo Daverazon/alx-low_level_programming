eb190588b35ffc9cbcd485de7de4c7c549c79181
HGT <kamsidavid2@gmail.com>
2023-03-16 17:44:55 -0700

    done

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -10,9 +10,10 @@ int main(void)
 	char quote[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
 	int i;
 
-	for (i = 0 ; i < 57 ; i++)
+	for (i = 0 ; i < 58 ; i++)
 	{
 		putchar(quote[i]);
 	}
+	putchar('\n');
 	return (1);
 }
