ebe3da6f9db90443669212d5de14d95e9f57edfe
HGT <kamsidavid2@gmail.com>
2023-04-03 13:51:04 -0700

    done

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -10,8 +10,8 @@
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int index;
-	unsigned char *destination = dest;
-	const unsigned char *source = src;
+	char *destination = dest;
+	const char *source = src;
 
 	for (index = 0; index < n; index++)
 		destination[index] = source[index];
