ed97670ae7272e16f388320b8031376c7576b4b8
HGT <kamsidavid2@gmail.com>
2023-04-11 06:05:22 -0700

    done

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -10,5 +10,20 @@
  */
 char *create_array(unsigned int size, char c)
 {
+	int i;
+	char *arr = (char *)malloc(sizeof(char) * size);
 
+	if (arr == NULL)
+		return (NULL);
+
+	for (i = 0; i < size; i++)
+		arr[i] = c;
+
+
+
+
+
+
+
+	return (arr);
 }
