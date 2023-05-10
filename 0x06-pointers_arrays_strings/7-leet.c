d51ed276ef41d37b01d4e70cbc5c90565642f76f
David_ubuntu <kamsidavid2@gmail.com>
2023-05-10 17:54:11 +0100

    improve code readability

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,3 +1,15 @@
+/*
+* Write a function that encodes a string into 1337.
+* Letters a and A should be replaced by 4
+* Letters e and E should be replaced by 3
+* Letters o and O should be replaced by 0
+* Letters t and T should be replaced by 7
+* Letters l and L should be replaced by 1
+* You can only use one if in your code
+* You can only use two loops in your code
+* You are not allowed to use switch
+* You are not allowed to use any ternary operation
+*/
 #include "main.h"
 
 /**
@@ -8,20 +20,16 @@
  */
 char *leet(char *str)
 {
-	int indx1 = 0, indx2;
-	char leet[8] = {'O', 'L', '?', 'E', 'A', '?', '?', 'T'};
+	char leet[] = "A4E3O0T7L1";
+	int index, check;
 
-	while (str[indx1])
+	for (index = 0; str[index]; index++)
 	{
-		for (indx2 = 0; indx2 <= 7; indx2++)
+		for (check = 0; leet[check]; check += 2)
 		{
-			if (str[indx1] == leet[indx2] ||
-			    str[indx1] - 32 == leet[indx2])
-				str[indx1] = indx2 + '0';
+			if (str[index] == leet[check] || str[index] == leet[check] + 32)
+				str[index] = leet[check + 1];
 		}
-
-		indx1++;
 	}
-
 	return (str);
 }
