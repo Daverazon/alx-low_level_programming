97094bba57cad8c30328e57254bf4dcc48386600
David_ubuntu <kamsidavid2@gmail.com>
2023-05-17 22:27:10 +0100

    add updated complex codes

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -19,9 +19,9 @@ char *str_concat(char *s1, char *s2)
 {
 	int size = 0, size1 = 0;
 	char *str;
-	if (!*s1)
+	if (s1 == NULL)
 		s1 = "";
-	if (!*s2)
+	if (s2 == NULL)
 		s2 = "";
 
 	while (s1[size++])
