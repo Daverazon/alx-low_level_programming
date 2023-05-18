ba1bd4f9bf959421b429b3b97d3261c5e0ecf448
David_ubuntu <kamsidavid2@gmail.com>
2023-05-18 14:26:21 +0100

    done

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -1,18 +1,18 @@
 /*
-* Write a function that reallocates a memory block using malloc and free
-* where ptr is a pointer to the memory previously allocated with a call to
-* malloc: malloc(old_size), old_size is the size, in bytes, of the allocated
-* space for ptr and new_size is the new size, in bytes of the new memory block
-* The contents will be copied to the newly allocated space, in the range from
-* the start of ptr up to the minimum of the old and new sizes
-* If new_size > old_size, the “added” memory should not be initialized
-* If new_size == old_size do not do anything and return ptr
-* If ptr is NULL, then the call is equivalent to malloc(new_size), for all
-* values of old_size and new_size
-* If new_size is equal to zero, and ptr is not NULL, then the call is equivalent
-* to free(ptr). Return NULL
-* Don’t forget to free ptr when it makes sense
-*/
+ * Write a function that reallocates a memory block using malloc and free
+ * where ptr is a pointer to the memory previously allocated with a call to
+ * malloc: malloc(old_size), old_size is the size, in bytes, of the allocated
+ * space for ptr and new_size is the new size, in bytes of the new memory block
+ * The contents will be copied to the newly allocated space, in the range from
+ * the start of ptr up to the minimum of the old and new sizes
+ * If new_size > old_size, the “added” memory should not be initialized
+ * If new_size == old_size do not do anything and return ptr
+ * If ptr is NULL, then the call is equivalent to malloc(new_size), for all
+ * values of old_size and new_size
+ * If new_size is equal to zero, and ptr is not NULL, then the call is
+ * equivalent to free(ptr). Return NULL
+ * Don’t forget to free ptr when it makes sense
+ */
 #include "main.h"
 #include <stdlib.h>
 
@@ -26,5 +26,35 @@
  */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
+	unsigned int index;
+	char *memset;
+	void *newptr;
 
+	if (new_size == old_size)
+		return (ptr);
+
+	if (ptr == NULL)
+	{
+		newptr = malloc(new_size);
+		if (newptr == NULL)
+			return (NULL);
+		return (newptr);
+	}
+
+	if (new_size == 0 && ptr != NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+
+	newptr = malloc(new_size);
+	if (newptr == NULL)
+		return (NULL);
+	memset = newptr;
+
+	for (index = 0; index < old_size; index++)
+		memset[index] = 0;
+
+	free(ptr);
+	return (newptr);
 }
