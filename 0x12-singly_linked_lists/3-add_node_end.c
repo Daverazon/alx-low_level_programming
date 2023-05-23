eed9e444b586ce472259cf2ca549c7222061ebcd
David_ubuntu <kamsidavid2@gmail.com>
2023-05-23 21:21:21 +0100

    done

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,3 +1,10 @@
+/*
+* Write a function that adds a new node at the end of a list_t list.
+* Prototype: list_t *add_node_end(list_t **head, const char *str);
+* Return: the address of the new element, or NULL if it failed
+* str needs to be duplicated
+* You are allowed to use strdup
+*/
 #include "lists.h"
 #include <string.h>
 
@@ -12,6 +19,24 @@
  */
 list_t *add_node_end(list_t **head, const char *str)
 {
+	list_t *newNode = malloc(sizeof(list_t));
+	list_t *lastNode = *head;
 
+	if (!newNode)
+		return (NULL);
+	newNode->next = NULL;
+	newNode->str = strdup(str);
+	if (newNode->str == NULL)
+		return (NULL);
+	newNode->len = strlen(str);
+	if (!*head)
+	{
+		*head = newNode;
+		return (newNode);
+	}
+	while (lastNode->next)
+		lastNode = lastNode->next;
+	lastNode->next = newNode;
 
+	return (newNode);
 }
--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -1,3 +1,7 @@
+/*
+* Write a function that frees a list_t list.
+* Prototype: void free_list(list_t *head);
+*/
 #include "lists.h"
 #include <stdlib.h>
 
@@ -7,3 +11,13 @@
  */
 void free_list(list_t *head)
 {
+	list_t *temp;
+
+	while (head)
+	{
+		temp = head->next;
+		free(head->str);
+		free(head);
+		head = temp;
+	}
+}
