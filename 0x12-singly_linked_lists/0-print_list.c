3e028ad86ef9965fa7a8c4fd99ab38ee84ac5e1f
David_ubuntu <kamsidavid2@gmail.com>
2023-05-23 18:08:59 +0100

    done

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,28 +1,30 @@
+/*
+* Write a function that prints all the elements of a list_t list.
+* Prototype: size_t print_list(const list_t *h);
+* Return: the number of hs
+* If str is NULL, print [0] (nil)
+* You are allowed to use printf
+*/
 #include "lists.h"
-#include <stdio.h>
 
 /**
  * print_list - Prints all the elements of a list_t list.
  * @h: The list_t list.
  *
- * Return: The number of nodes in h.
+ * Return: The number of hs in h.
  */
 
 size_t print_list(const list_t *h)
 {
-	size_t nodes = 0;
-
+	size_t count = 0;
 	while (h)
 	{
 		if (h->str == NULL)
 			printf("[0] (nil)\n");
-
 		else
 			printf("[%d] %s\n", h->len, h->str);
-
-		nodes++;
 		h = h->next;
+		count++;
 	}
-
-	return (nodes);
+	return (count);
 }
