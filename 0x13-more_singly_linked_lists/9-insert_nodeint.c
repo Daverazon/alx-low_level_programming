d9ce0e28ccdffccc65ff4caae70646ee99dd99bb
David_ubuntu <kamsidavid2@gmail.com>
2023-05-24 20:38:28 +0100

    done

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -31,7 +31,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	if (!newNode)
 		return (NULL);
 	newNode->n = n;
-	if (*head && idx == 0)
+	if (!*head && idx == 0)
 	{
 		newNode->next = *head;
 		*head = newNode;
