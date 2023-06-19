f8c7009d7214986bd73bb2532bc0e8b5b6f1a342
David_ubuntu <kamsidavid2@gmail.com>
2023-06-19 21:59:59 +0100

    add dynamic library

---
+++ 0x18-dynamic_libraries/Cfiles/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _isupper - checks for capital letters
+ * @c: letter to be checked
+ *
+ * Return: returns 1 if capital or 0 if not
+ */
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+	else
+		return (0);
+}
---
+++ 0x18-dynamic_libraries/Cfiles/100-atoi.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _atoi - converts a string to an integer
+ * @s: string to be converted
+ * Return: the integer
+ */
+int _atoi(char *s)
+{
+	unsigned int num = 0;
+	int sign = 1, i = 0;
+
+	while (*(s + i))
+	{
+		if (*(s + i) == '-')
+			sign *= -1;
+		if (*(s + i) >= '0' && *(s + i) <= '9')
+			num = num * 10 + (*(s + i) - '0');
+		else if (num > 0)
+			break;
+		i++;
+	}
+	return (sign * num);
+}
