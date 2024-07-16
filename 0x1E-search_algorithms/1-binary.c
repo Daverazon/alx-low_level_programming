b8e103e8602475baad31e537dde3b6c79819d7e3
DavidI <kamsidavid2@gmail.com>
2024-07-16 20:08:15 +0100

    create a binary sort algorithm

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -15,5 +15,37 @@
  */
 int binary_search(int *array, size_t size, int value)
 {
+	size_t idx0 = 0, idx1, idx;
 
+	if (array == NULL || size == 0)
+		return (-1);
+
+	idx1 = size - 1;
+	while (1)
+	{
+		printf("Searching in array: %d", array[idx0]);
+		for (size = idx0 + 1; size <= idx1; size++)
+		{
+			printf(", ");
+			printf("%d", array[size]);
+		}
+		putchar('\n');
+
+		idx = (idx0 + idx1) / 2;
+		if (array[idx] == value)
+			return (idx);
+		if (value < array[idx])
+		{
+			if (idx == idx0)
+				break;
+			idx1 = idx -= 1;
+		}
+		if (value > array[idx])
+		{
+			if (idx == idx1)
+				break;
+			idx0 = idx += 1;
+		}
+	}
+	return (-1);
 }
